77b95a1b77f4727f97ecaec4dd2a3d5b9c84a358
Ines de Santiago <InesdeSantiago@users.noreply.github.com>
2017-02-22 16:25:31 +0000

    Create split_Fasta_.py

---
+++ split_Fasta_.py
@@ -0,0 +1,18 @@
+import sys, os
+
+input_fa = sys.argv[1]
+output_dir = sys.argv[2]
+
+#!/usr/bin/env python
+f=open(input_fa,"r");
+opened = False
+for line in f :
+    if(line[0] == ">") :
+        if(opened) :
+            of.close()
+        opened = True
+        outname = os.path.join(output_dir,line[1:].rstrip()))
+        of=open("%s.fa" % (outname, "w")
+        print(line[1:].rstrip())
+    of.write(line)
+of.close()
